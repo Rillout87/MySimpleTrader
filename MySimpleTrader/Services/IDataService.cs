609adfb5c935746e1ef6115fae302b53b4ff235e
Kirill Fedorov <fedkv2013@gmail.com>
2023-01-27 15:30:00 +0300

    Добавьте файлы проекта.

---
+++ MySimpleTrader/Services/IDataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySimpleTrader.Domain.Services
+{
+    internal interface IDataService<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetIndexBinder(int id);
+        Task<T> Update(int id, T entity);
+        Task<T> Create(T entity);
+        Task <bool> Delete(int id);
+    }
+}
---
+++ SimpleTrader.EntityFramework/SimpleTraderDbContext.cs
@@ -0,0 +1,25 @@
+﻿using MySimpleTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.EntityFramework
+{
+    internal class SimpleTraderDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+            
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
